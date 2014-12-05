69b12b82b1451411a34a3c4ac3b3849891c1c856
PiePieNL <pavanriel@gmail.com>
2014-12-05 11:19:54 +0100

    project toegevoegd

---
+++ eigenopdracht/eigenopdracht/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="eigenopdracht.Global" Language="C#" %>
---
+++ eigenopdracht/eigenopdracht/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(eigenopdracht.Startup))]
+namespace eigenopdracht
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
