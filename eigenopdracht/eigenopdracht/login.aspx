d2f8ff92b57473a476724139c92db4742848a4ae
PiePieNL <pavanriel@gmail.com>
2014-12-26 17:54:01 +0100

    Login gemaakt
    login webform gemaakt en methoden voor checken username en een methode
    voor password

---
+++ eigenopdracht/eigenopdracht/login.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="login.aspx.cs" Inherits="eigenopdracht.login" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+
+    
+
+    <div>
+        <h1>Login</h1>
+    </div>
+    <div style="text-align: left">
+
+        <table style="width: 100%;">
+            <tr>
+                <td class="text-left" style="width: 223px; text-align: right">Username</td>
+                <td class="text-left" style="width: 434px">
+                    <asp:TextBox ID="txtUsername" runat="server"></asp:TextBox>
+                    <asp:RequiredFieldValidator ID="rfUsername" runat="server" ControlToValidate="txtUsername" ErrorMessage="Username required" ForeColor="Red"></asp:RequiredFieldValidator>
+                </td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td class="text-left" style="width: 223px; height: 27px; text-align: right">Password</td>
+                <td class="text-left" style="height: 27px; width: 434px">
+                    <asp:TextBox ID="txtPassword" runat="server"></asp:TextBox>
+                    <asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" ControlToValidate="txtPassword" ErrorMessage="Password required" ForeColor="Red"></asp:RequiredFieldValidator>
+                </td>
+                <td style="height: 27px"></td>
+            </tr>
+            <tr>
+                <td class="text-left" style="width: 223px; text-align: right">
+                    <asp:Label ID="lbErrormes" runat="server" ForeColor="Red" style="text-align: right" Visible="False"></asp:Label>
+                </td>
+                <td class="text-left" style="width: 434px">
+                    <asp:Button ID="btnLogin" runat="server" OnClick="btnLogin_Click" Text="Login" />
+                </td>
+                <td>&nbsp;</td>
+            </tr>
+        </table>
+    </div>
+
+    
+
+</asp:Content>
---
+++ eigenopdracht/eigenopdracht/login.aspx.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Oracle.DataAccess.Client;
+using Oracle.DataAccess.Types;
+
+
+namespace eigenopdracht
+{
+    public partial class login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            if(checkusename() == true)
+            {
+                if(checkpass()==true)
+                {
+                    Session["Username"] = txtUsername.Text;
+                }
+                else
+                {
+                    lbErrormes.Text = "password incorrect";
+                    lbErrormes.Visible = true;
+                }
+            }
+            else
+            {
+                lbErrormes.Text = "Username incorect";
+                lbErrormes.Visible = true;
+            }
+
+        }
+
+        public bool checkusename()
+        {
+            bool userbestaat = false;
+            OracleConnection conn = new OracleConnection(System.Configuration.ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
+            int usernamecheck;
+            try
+            {
+                conn.Open();
+                OracleCommand oraCommand = new OracleCommand("SELECT count(*) as usercheck from ACCOUNT where username = :username", conn);
+                oraCommand.Parameters.Add(new OracleParameter("username", txtUsername.Text));
+                usernamecheck = int.Parse(oraCommand.ExecuteScalar().ToString());
+                if( usernamecheck ==1)
+                {
+                    userbestaat = true;
+                }
+
+            }
+            catch { }
+            finally
+            {
+                conn.Close();
+            }
+            return userbestaat;
+        }
+
+        public bool checkpass()
+        {
+            bool passcorrect = false;
+            OracleConnection conn = new OracleConnection(System.Configuration.ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
+            try
+            {
+                conn.Open();
+                OracleCommand oraCommand = new OracleCommand("SELECT wachtwoord  from ACCOUNT where username = :username", conn);
+                oraCommand.Parameters.Add(new OracleParameter("username", txtUsername.Text));
+                string password = oraCommand.ExecuteScalar().ToString();
+                if (password == txtPassword.Text)
+                {
+                    passcorrect = true;
+                }
+
+            }
+            catch { }
+            finally
+            {
+                conn.Close();
+            }
+            return passcorrect;
+
+        }
+
+     
+    }
+}
